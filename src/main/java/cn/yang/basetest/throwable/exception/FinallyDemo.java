c508d8e1a71ac42a81c0b04ff7720a7b83e28a7d
ityy <ofyang@qq.com>
2018-07-25 15:07:59 +0800

    更新异常测试-finally

---
+++ src/main/java/cn/yang/basetest/throwable/exception/FinallyDemo.java
@@ -0,0 +1,28 @@
+package cn.yang.basetest.throwable.exception;
+
+public class FinallyDemo {
+    public static void main(String[] args) {
+
+        FinallyDemo finallyDemo = new FinallyDemo();
+        System.out.println("主程序中: " + finallyDemo.returnTest(100));
+        /*
+            finally中: 200
+            主程序中: 100
+            因为finally始终会执行, 所以try中返回了100, 但finally中还是会执行
+            可以理解为try中是个快照
+         */
+
+    }
+
+    public int returnTest(int a) {
+        try {
+            return a;
+        } catch (Exception e) {
+
+        } finally {
+            a = a + a;
+            System.out.println("finally中: " + a);
+        }
+        return 0;
+    }
+}
